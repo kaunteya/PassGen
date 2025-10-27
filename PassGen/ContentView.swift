c5065492241b1661e7468c8a31a5b3d9fc788336
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2025-10-27 15:26:27 +0530

    Initial Commit

---
+++ PassGen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PassGen
+//
+//  Created by Kauntey Suryawanshi on 27/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PassGen/PassGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassGenApp.swift
+//  PassGen
+//
+//  Created by Kauntey Suryawanshi on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PassGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
